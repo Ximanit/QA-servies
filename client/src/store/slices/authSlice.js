2fe3c69fc835034410189037f7ca4acefd309fbf
Ximanit <122241859+Ximanit@users.noreply.github.com>
2025-04-23 01:29:23 +0000

    Начал настраивать email в профиле

--- client/src/store/slices/authSlice.js
+++ client/src/store/slices/authSlice.js
@@ -6,6 +6,7 @@ const initialState = {
 	token: localStorage.getItem('token') || null,
 	roles: localStorage.getItem('roles') || null,
 	id: localStorage.getItem('id') || null,
+	email: localStorage.getItem('email') || null,
 };
 
 const authSlice = createSlice({
@@ -17,20 +18,24 @@ const authSlice = createSlice({
 			state.token = action.payload.data.token;
 			state.roles = JSON.stringify(action.payload.data.roles);
 			state.id = action.payload.data.id;
+			state.email = action.payload.data.email;
 			localStorage.setItem('user', action.payload.data.name);
 			localStorage.setItem('token', action.payload.data.token);
 			localStorage.setItem('roles', JSON.stringify(action.payload.data.roles));
 			localStorage.setItem('id', action.payload.data.id);
+			localStorage.setItem('email', action.payload.data.email);
 		},
 		logout: (state) => {
 			state.user = null;
 			state.token = null;
 			state.roles = null;
 			state.id = null;
+			state.email = null;
 			localStorage.removeItem('user');
 			localStorage.removeItem('token');
 			localStorage.removeItem('roles');
 			localStorage.removeItem('id');
+			localStorage.removeItem('email');
 		},
 	},
 });
